28158924de12f5f4e3afe9c9296fed7f214bce91
fxnoob <fxnoob71@gmail.com>
2018-08-19 11:28:40 +0530

    utils: improving char_to_num utility.

--- expression_eval/utils.h
+++ expression_eval/utils.h
@@ -5,13 +5,23 @@
 #ifndef EXPRESSION_EVAL_UTILS_H
 #define EXPRESSION_EVAL_UTILS_H
 
+#include <ctype.h>
+
+#define PLUS_OPERATOR '+'
+#define MINUS_OPERATOR '-'
+#define MULTI_OPERATOR '*'
+#define DIV_OPERATOR '/'
+#define POW_OPERATOR '^'
+#define LEFT_BRACKET '('
+#define RIGHT_BRACKET ')'
 
 
-int chars_to_num(int from, int to, char *array){
-    int decimal = 0,init_count=(int)'0';
-    for (int i = from; i <= to; i++) {
+int chars_to_num(int from, int *toIndex, char *array){
+    int decimal = 0,i,init_count=(int)'0';
+    for (i = from; isdigit(array[i])&&array[i]!='\0'; i++) {
         decimal = decimal*10 + (array[i] - init_count);
     }
+    *toIndex = i;
     return decimal;
 }
 
